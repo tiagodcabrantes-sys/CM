ff4b0eab30a84bcbebfb698e186935e2ca76481b
Tiago.Abrantes <tiagodc.abrantes@gmail.com>
2026-03-17 01:08:00 +0000

    Adicionei mensagem de entrada na aplicacao HelloWorld

--- Trabalho1/HelloWorld/app/src/main/java/cm_aXXXXX/helloworld/MainActivity.kt
+++ Trabalho1/HelloWorld/app/src/main/java/cm_aXXXXX/helloworld/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Welcome to my App!",Toast.LENGTH_LONG).show()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
